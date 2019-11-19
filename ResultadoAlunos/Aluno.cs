ffa35b13025aed597627021614685084567dbe21
Lucas Lacerda <lucaslmzc29@gmail.com>
2019-11-19 14:43:55 -0300

    Projeto iniciado, recebe 3 notas e informa se o aluno foi aprovado, caso não tenha sido, mostra quantos pontos faltaram

---
+++ ResultadoAlunos/Aluno.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ResultadoAlunos
+{
+    class Aluno
+    {
+        public string Nome;
+        public double Nota1, Nota2, Nota3;
+
+        public double NotaFinal()
+        {
+            return Nota1 + Nota2 + Nota3;
+        }
+
+        public bool Aprovado()
+        {
+            if (NotaFinal() >= 60)
+                return true;
+            return false;
+        }
+
+        public double PontosNecessarios()
+        {
+            if(Aprovado())
+                return 0;
+            return 60 - NotaFinal();
+        }
+    }
+}
---
+++ ResultadoAlunos/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ResultadoAlunos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno a = new Aluno();
+
+            Console.Write("Aluno \nNome:");
+            a.Nome = Console.ReadLine();
+            Console.Write("Digite a 1º nota (máx = 30): ");
+            a.Nota1 = double.Parse(Console.ReadLine());
+            Console.Write("Digite a 2º nota (máx = 35): ");
+            a.Nota2 = double.Parse(Console.ReadLine());
+            Console.Write("Digite a 1º nota (máx = 35): ");
+            a.Nota3 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nNota Final: {0:F2}", a.NotaFinal());
+            if(a.Aprovado())
+                Console.WriteLine("Aprovado");
+            else
+            {
+                Console.WriteLine("Reprovado");
+                Console.WriteLine("Faltou {0:F2} pontos", a.PontosNecessarios());
+            }
+
+        }
+    }
+}
